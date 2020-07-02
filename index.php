c21087c39b9df90d1d326549507e05ba74a0aa8d
pratikshachamute1998 <chamutepratiksha1998@gmail.com>
2020-07-02 19:28:58 +0530

    added new link

--- index.php
+++ index.php
@@ -59,6 +59,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact Us</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">New Features</a>
+                </li>
             </ul>
         </nav>
 
